023ebfc07beab02023e75468ad12e231b23f81d4
Doerthe Kosicki <kiki@codestun.com>
2020-08-11 14:39:54 +0200

    add routes for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get all
+  get "tasks", to: "tasks#index"
+  # form for new one
+  get "tasks/new", to: "tasks#new", as: :new_task
+  # for one
+  get "tasks/:id", to: "tasks#show", as: :task
+  # receive POST for creating record
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
 end
