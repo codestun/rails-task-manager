92cbc1c1f6b76135b3737861c67e913f4a7a956a
Doerthe Kosicki <kiki@codestun.com>
2020-08-11 15:40:33 +0200

    update create method and add update method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,18 +12,24 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(tasks_params)
-    @task = Task.save
-    redirect_to task_path(@tasks)
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
   end
 
   def edit
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
-  def restaurant_params
+  def task_params
     params.require(:task).permit(:title, :details, :confirmed)
   end
 end
