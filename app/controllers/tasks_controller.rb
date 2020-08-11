4fa897472c0a65c2648804709dd632edbab7e46b
Doerthe Kosicki <kiki@codestun.com>
2020-08-11 14:39:02 +0200

    update controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,29 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task = Task.save
+    redirect_to task_path(@tasks)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:task).permit(:title, :details, :confirmed)
+  end
 end
