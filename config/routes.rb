d3d872c5fe94b1a7d3aa0efafe52b188ad034211
Doerthe Kosicki <kiki@codestun.com>
2020-08-11 15:41:46 +0200

    add routes for update

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # receive POST for creating record
   post "tasks", to: "tasks#create"
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
 end
